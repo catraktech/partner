b4354cef3fc1f20d58b9afe7155f25e98a0db28c
Taher <Dheeraj117@github.com>
2023-12-20 12:56:29 +0530

    update repo

--- src/main/java/com/catrak/exatip/partner/repository/ManufacturerRepository.java
+++ src/main/java/com/catrak/exatip/partner/repository/ManufacturerRepository.java
@@ -3,6 +3,7 @@ package com.catrak.exatip.partner.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.catrak.exatip.commonlib.entity.Manufacturer;
@@ -10,6 +11,7 @@ import com.catrak.exatip.commonlib.entity.Manufacturer;
 @Repository
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
 
+    @Query(value = "select * from manufacturer m where lower(m.\"name\") = lower(?1)", nativeQuery = true)
     Optional<Manufacturer> findByName(String manufacturerName);
 
 }
